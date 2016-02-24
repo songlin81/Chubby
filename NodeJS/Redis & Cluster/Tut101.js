30a947827afa218dce9f5aee891f2957df8c1d50
Lin, Song <koalainuwo@hotmail.com>
2016-02-24 11:56:25 +0800

    Delete Tut101.js

--- NodeJS/Redis & Cluster/Tut101.js
+++
@@ -1,6 +0,0 @@
-var http = require("http");
-http.createServer(function(request, response) {
-    console.log(process.pid + ": request for " + request.url);
-    response.writeHead(200);
-    response.end("Hello World!");
-}).listen(8000);
